7eef5119e4e1513d7d33411b52cd140e71f2c219
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-04-01 13:04:09 +0200

    Update myClass2.cs
    slettet koden i myclass2

--- myClass2.cs
+++ myClass2.cs
@@ -2,26 +2,7 @@ namespace GitExercise
 {
     class myClass2
     {
-        private string _name;
-        private string _adress;
-        private string _by;
-        public myClass2(string name,string adress,string by)
-        {
-            _name = name;
-            _adress = adress;
-            _by = by;           
-        }
-        public string name
-        {
-            get{ return _name; }
-        }
-        public string adress
-        {
-            get { return _adress;}
-        }
-        public string by
-        {
-            get { return _by;}
+       
         }
         public override string ToString()
         {
