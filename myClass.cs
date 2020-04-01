7fe3e68147f284163a03c3f3761611632559a8b0
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-04-01 10:45:40 +0200

    Tilføjet instance field, constructer og properties
    Navn

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,14 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        private string _name;
+        public myClass(string name)
+        {
+            _name = name;
+        }
+        public string name
+        {
+            get{ return _name; }
+        }
     }
 }
