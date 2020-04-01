2d361e951a8401501bd61db2f5654f78780a75c9
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-04-01 11:06:24 +0200

    Tilføjet objekter og writeline
    3 objekter og writeline der udskriver deres navn og adresse

--- Program.cs
+++ Program.cs
@@ -8,8 +8,13 @@ namespace GitExercise
         {
             Console.WriteLine("Hello World!");
 
-            myClass myclass = new myClass();
+            myClass myclass = new myClass("Simon","Hvidovrevej 312");
+            myClass myclass2 = new myClass("Bo","Albertslund hovedgade 31");
+            myClass myclass3 = new myClass("Per","Taastrup allé 201");
             
+            Console.WriteLine($"{myclass}");
+            Console.WriteLine($"{myclass2}");
+            Console.WriteLine($"{myclass3}");
 
 
         }
